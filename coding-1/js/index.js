4814235936b0880308f48af0156fd3474075c2a4
Tai Nguyen <thtai2304@gmail.com>
2021-04-25 00:54:14 +0700

    codind-6 va coding-bootrap

--- coding-1/js/index.js
+++ coding-1/js/index.js
@@ -1,21 +1,27 @@
-const accordionList = document.getElementsByClassName('accordion');
-if (accordionList) {
-    for (let accordion of accordionList) {
-        accordion.onclick = function () {
-            this.classList.toggle('active');
-            let panel = this.nextElementSibling;
-            if (panel.style.display === 'block') {
-                panel.style.display = 'none';
-            }
-            else {
-                panel.style.display = 'block';
-            }
-            if (panel.style.maxHeight) {
-                panel.style.maxHeight = null;
-            }
-            else {
-                panel.style.maxHeight = panel.scrollHeight + 'px';
-            }
-        }
-    }
-}
+// const accordionList = document.getElementsByClassName('accordion');
+// if (accordionList) {
+//     for (let accordion of accordionList) {
+//         accordion.onclick = function () {
+//             this.classList.toggle('active');
+//             let panel = this.nextElementSibling;
+//             if (panel.style.display === 'block') {
+//                 panel.style.display = 'none';
+//             }
+//             else {
+//                 panel.style.display = 'block';
+//             }
+//             if (panel.style.maxHeight) {
+//                 panel.style.maxHeight = null;
+//             }
+//             else {
+//                 panel.style.maxHeight = panel.scrollHeight + 'px';
+//             }
+//         }
+//     }
+// }
+$(document).ready(function(){
+    $(".accordion").click(function(){
+        $(this).next().toggle();
+        $(this).toggleClass('active')
+    });
+});
